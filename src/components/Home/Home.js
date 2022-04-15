70b4a13fcbbeddd6f5a61769b8819bca3beb4b26
ShareefRahat <imr4h47@gmail.com>
2022-04-16 00:50:35 +0600

    home hero section updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,32 @@
 import React from "react";
+import bgBanner from "../../images/bannerbackground.png";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section
+        style={{
+          backgroundImage: `url(${bgBanner})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+        className="bg-cover h-[150px] w-full md:h-[570px] flex justify-center items-center"
+      >
+        <div>
+          <input
+            className="outline-none border-2 rounded-full border-slate-300 focus:border-orange-600 px-4  py-1 text-md md:text-xl text-green-700 font-serif w-[250px] md:w-[500px]"
+            type="text"
+            placeholder="Search your meal"
+          />
+          <span className="bg-green-600 hover:bg-orange-600 text-white px-4 py-2 mx-2 text-md md:text-xl rounded-full font-serif cursor-pointer">
+            Search
+          </span>
+        </div>
+      </section>
+      <section>Meal components</section>
+      <section>Features card</section>
+    </div>
+  );
 };
 
 export default Home;
