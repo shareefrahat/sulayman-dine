b81d08e82f6ab540a459ab5b9453b6df5702ee84
ShareefRahat <imr4h47@gmail.com>
2022-04-15 19:56:00 +0600

    header ui design completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Tailwind</h1>
+      <Header></Header>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../../images/logo.jpg";
+import { ShoppingCartIcon } from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex flex-row justify-between items-center py-3 md:py-4 px-5 md:px-10 shadow-lg ">
+        <div>
+          <img className="w-32 md:w-36" src={logo} alt="" />
+        </div>
+        <div className="flex flex-row justify-center items-center ">
+          <div>
+            <button className="mx-5 md:mx-10 relative mt-2">
+              <span className="bg-orange-600 text-white aspect-square rounded-full absolute bottom-4 left-4 px-1 font-bold">
+                0
+              </span>
+              <ShoppingCartIcon className="w-8 text-slate-500 hover:text-orange-600 font-thin"></ShoppingCartIcon>
+            </button>
+          </div>
+          <div>
+            <button className="rounded-full bg-green-600 text-white font-s font-bold text-lg px-5 py-1 hover:bg-orange-600">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
